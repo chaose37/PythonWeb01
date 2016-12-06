87bb21ec9e5388fba5ebe57dab49fcac17a59b5f
choi <chaose37@naver.com>
2016-12-06 19:29:04 +0900

    161206

--- myApp01/views.py
+++ myApp01/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+
+def index(req):
+    msg = "My Message"
+    return render(req,'index.html',{'msg':msg})
 # Create your views here.
